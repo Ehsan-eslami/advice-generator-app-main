ab76fdefb5a8a93af9d402b516500c231f9e3b83
EHSAN\esehs <eslami.ehsan200@gmail.com>
2024-01-20 12:10:00 +0330

    Release version

---
+++ scripts.js
@@ -0,0 +1,29 @@
+(function () {
+  "use strict";
+  // Get the elements
+  const adviceId = document.querySelector("#adviceId");
+  const adviceText = document.querySelector("#adviceText");
+  const adviceBtn = document.querySelector("#adviceBtn");
+  const apiUrl = "https://api.adviceslip.com/advice";
+
+  // Get advice from API
+  function getAdvice() {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          console.error("error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        adviceId.textContent = `#${data.slip.id}`;
+        adviceText.textContent = `"${data.slip.advice}"`;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  getAdvice();
+  adviceBtn.addEventListener("click", getAdvice);
+})();
